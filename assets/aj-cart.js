976e5fbd6810620b156446f76735e00c54b33cbc
CodeNoob13 <116080315+CodeNoob13@users.noreply.github.com>
2025-08-16 00:18:09 +0200

    cart discount box done

--- assets/aj-cart.js
+++ assets/aj-cart.js
@@ -2,6 +2,7 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("ajCart", () => ({
     cartPrice: 0,
     cartDiscount: 0,
+    cartDiscounts: [],
     init() {
       this.fetchCart();
       document.addEventListener("cart:refresh", () => {
@@ -23,7 +24,24 @@ document.addEventListener("alpine:init", () => {
         .then((cart) => {
           this.cartPrice = cart.total_price / 100;
           this.cartDiscount = cart.total_discount / 100;
+          this.cartDiscounts = cart.cart_level_discount_applications || [];
         });
     },
   }));
+
+  // Add ajCart to Alpine store for global access
+  Alpine.store("ajCart", {
+    cartPrice: 0,
+    cartDiscount: 0,
+    cartDiscounts: [],
+    fetchCart() {
+      fetch("/cart.js")
+        .then((res) => res.json())
+        .then((cart) => {
+          this.cartPrice = cart.total_price / 100;
+          this.cartDiscount = cart.total_discount / 100;
+          this.cartDiscounts = cart.cart_level_discount_applications || [];
+        });
+    },
+  });
 });
