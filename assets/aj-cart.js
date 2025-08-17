25d20287d84dd0902302263e101a7e77706d1d8a
CodeNoob13 <116080315+CodeNoob13@users.noreply.github.com>
2025-08-17 20:24:05 +0200

    start of compare at price

--- assets/aj-cart.js
+++ assets/aj-cart.js
@@ -1,10 +1,10 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("ajCart", () => ({
     cartPrice: 0,
-    cartWithoutDiscount: 0,
     cartDiscount: 0,
-
+    cartProducts: [],
     allProducts: [],
+    totalComparePrice: 0,
 
     init() {
       this.fetchCart();
@@ -21,21 +21,56 @@ document.addEventListener("alpine:init", () => {
       });
     },
     fetchCart() {
+      this.fetchAllProducts();
+
       fetch("/cart.js")
         .then((res) => res.json())
         .then((cart) => {
           this.cartPrice = cart.total_price / 100;
           this.cartDiscount = cart.total_discount / 100;
-          this.cartWithoutDiscount = this.cartPrice + this.cartDiscount;
-          this.fetchProducts();
-          console.log(this.cartWithoutDiscount);
+          this.cartProducts = cart.items.map((item) => {
+            const product = this.allProducts.find((p) => p.id === item.id);
+            return {
+              ...item,
+              ...product,
+            };
+          });
+
+          this.allProducts.forEach((product) => {
+            console.log(product.variants);
+          });
+
+          // Get total compare at price
+
+          this.getTotalComparePrice();
+          console.log((this.totalComparePrice / 100).toFixed(2));
         });
     },
-    fetchProducts() {
+
+    async getTotalComparePrice() {
+      let totalComparePrice = 0;
+
+      this.cartProducts.forEach((item) => {
+        if (item.compare_at_price && item.quantity === 1) {
+          totalComparePrice += item.compare_at_price;
+        } else if (item.compare_at_price && item.quantity > 1) {
+          totalComparePrice += item.compare_at_price * item.quantity;
+        } else {
+          totalComparePrice += item.line_price;
+        }
+      });
+      this.totalComparePrice = totalComparePrice;
+    },
+
+    fetchAllProducts() {
       fetch("/products.json")
-        .then((res) => res.json())
+        .then((response) => response.json())
         .then((data) => {
-          this.allProducts = data.products[0];
+          this.allProducts = data.products;
+          // console.log("All products:", this.allProducts);
+        })
+        .catch((error) => {
+          console.error("Error fetching products:", error);
         });
     },
   }));
